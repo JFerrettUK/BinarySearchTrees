0540a9589ec9f8e38f1ba132aecf599d3104a816
JFerrettUK <jferrettuk@gmail.com>
2023-03-18 13:13:21 +0000

    Binary search tree branches initialised

--- main.js
+++ main.js
@@ -1,36 +1,52 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-let start = 0;
 
-class binaryST {
-    constructor() {
-        this.root = null
+class TreeNode {
+    constructor(data, leftChild = null, rightChild = null) {
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+    addLeftNode = function(newNode) {
+        this.leftChild = new TreeNode(newNode)
+    }
+
+    addRightNode = function(newNode) {
+        this.rightChild = new TreeNode(newNode)
+    }
+}
+
+let array = [1, 2, 3, 4, 5, 6, 7]
+
+class Tree {
+    constructor(root) {
+        this.root = root
         this.size = 0;
     }
-
-    createBST = function(start, array) {
-        let end = (array.length - 1) 
-        let midNode = Math.round(((start + end) / 2))
-
-        let leftArray = array.slice(0, midNode);
-        let rightArray = array.slice(midNode, array.length);
-
-        if (rightArray.length == 1) {
-            let leftBranch = new treeNode(leftArray)
-            let rightBranch = new treeNode(rightArray)
-            return leftBranch, rightBranch;
-        }
-        
-        return this.createBST(start, leftArray), this.createBST(start, rightArray)
-    }
 }
 
-class treeNode {
-    constructor(data, branch = null) {
-        this.data = data;
-        this.branch = branch;
+let nodeA = new TreeNode()
+
+let i = 0
+
+let buildTree = function(array, nodeA, i) {
+    console.log("called " + i++)
+    console.log(nodeA)
+
+    if (array.length == 0) {
+        return array
     }
+    
+    let end = (array.length - 1) 
+    let midNodeNo = Math.round(((0 + end) / 2))
+    
+    nodeA.data = new TreeNode(array[midNodeNo])
+    nodeA.addLeftNode()
+    nodeA.addRightNode()
+
+    let leftArray = array.slice(0, midNodeNo);
+    let rightArray = array.slice(midNodeNo + 1, array.length);
+
+    return buildTree(leftArray, nodeA.leftChild, i), buildTree(rightArray, nodeA.rightChild, i)
 }
 
-
-let test = new binaryST(start, array)
-console.log(test)
+buildTree(array, nodeA, i)
