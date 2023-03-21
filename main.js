97951e922648316f930612689ec35f9fe70f3404
JFerrettUK <jferrettuk@gmail.com>
2023-03-21 08:22:17 +0000

    Addition to js in tree function

--- main.js
+++ main.js
@@ -1,53 +1,50 @@
 class Tree {
-    constructor(root) {
-        this.root = root
+    constructor() {
+        this.root = null
         this.size = 0;
+    }
+
+    buildTree = function(array, node) {
+        if (array.length == 0) {
+            return 
+        }
+        
+        function findMid(array) {
+            let end = (array.length - 1) 
+            let mid = (0 + end) / 2
+            return mid
+        }
+    
+        let mid = findMid(array)
+        let leftArray = array.slice(0, mid);
+        let rightArray = array.slice(mid + 1, array.length);
+    
+        node = new TreeNode(array[mid])
+    
+        if (leftArray.length > 0) {
+            node.left = new TreeNode(leftArray[findMid(leftArray)])
+        }
+        
+        if (rightArray.length > 0) {
+            node.right = new TreeNode(rightArray[findMid(rightArray)])
+        }
+        console.log(node)
+    
+        this.size++
+
+        return this.buildTree(leftArray, node.left), this.buildTree(rightArray, node.right)
     }
 }
 
 class TreeNode {
-    constructor(data, left = null, right = null) {
+    constructor(data) {
         this.data = data;
-        this.left = left;
-        this.right = right;
+        this.left = null;
+        this.right = null;
     }
 }
 
 let array = [1, 2, 3, 4, 5, 6, 7]
-
-
-// let nodeA = new TreeNode()
-
-let i = 0
-
-let buildTree = function(array, nodeA) {
-    if (array.length == 0) {
-        return array
-    }
-    
-    let start = 0
-    let end = (array.length - 1) 
-    let mid = (start + end) / 2
-    // nodeA.data = array[mid]
-    let nodeA = new TreeNode(array[mid])
-
-    let leftArray = array.slice(start, mid);
-    let rightArray = array.slice(mid + 1, array.length);
-
-    console.log(nodeA)
-
-    return buildTree(leftArray, nodeA.left), buildTree(rightArray, nodeA.right)
-}
-
-buildTree(array, nodeA)
-
-
-    // let leftEnd = (leftArray.length - 1) 
-    // let leftMid = Math.round(((0 + leftEnd) / 2))
-    // nodeA.left = new TreeNode(leftArray[leftMid])
-
-    // let rightEnd = (rightArray.length - 1) 
-    // let rightMid = Math.round(((0 + rightEnd) / 2))
-    // nodeA.right = new TreeNode(rightArray[rightMid])
-
-    // console.log(nodeA)
+let testTree = new Tree
+testTree.buildTree(array, testTree.root)
+console.log(testTree)
